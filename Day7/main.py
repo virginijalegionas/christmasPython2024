1b9bd516515b89d89b8cb3f5040a131bdc51631e
virginijalegionas <123477224+virginijalegionas@users.noreply.github.com>
2024-12-10 17:36:30 +0200

    fixes on day7

--- Day7/main.py
+++ Day7/main.py
@@ -3,16 +3,23 @@ import re
 
 def fixPackages(packages):
     # pattern to find the element we should reverse first
-    pattern = r"\((\w*)\)"
-    if "(" in packages and ")" in packages:
-        packSplit = re.split(pattern, packages)
+    pattern = r"\(\w*\)"
+    packSplit = []
+    isMatch = re.search(pattern, packages)
+    while isMatch:
+        packSplit.append(packages[:isMatch.start()])
+        packSplit.append(packages[isMatch.start():isMatch.end()])
+        packSplit.append(packages[isMatch.end():])
+        packSplit[1] = packSplit[1].strip("(")
+        packSplit[1] = packSplit[1].strip(")")
         packSplit[1] = packSplit[1][::-1]
         packages = ''.join(packSplit)
-        packages = fixPackages(packages)
+        packSplit = []
+        isMatch = re.search(pattern, packages)
     return packages
 
 
-inputString = "abc(def(gh)i)jk"
+inputString = "(abc(def(gh)i)(jk))"
 fixedString = fixPackages(inputString)
 print(f"Package was: {inputString}")
 print(f"Fixed Package is: {fixedString}")
